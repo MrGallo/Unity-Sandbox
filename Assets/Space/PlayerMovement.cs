788a0808a3828183c98274bf2522e2560cc24e44
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2023-03-02 08:21:37 -0500

    Complete lateral movement

--- Assets/Space/PlayerMovement.cs
+++ Assets/Space/PlayerMovement.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float MaxSpeed = 5.0f;
+    public float LateralThrusterForce = 10f;
+    private Rigidbody2D rb;
+    private float horizontalInput;
+    private float drag = 0.3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
 
+    void FixedUpdate()
+    {
+        if (horizontalInput != 0.0f)
+        {
+            float force = horizontalInput * LateralThrusterForce;
+            rb.AddForce(new Vector2(force, 0) * Time.deltaTime, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.AddForce(new Vector2(-rb.velocity.x * drag, 0));
+        }
+        
+        float clampedVelocity = Mathf.Clamp(rb.velocity.x, -MaxSpeed, MaxSpeed);
+        rb.velocity = new Vector2(clampedVelocity, 0);
     }
 }
