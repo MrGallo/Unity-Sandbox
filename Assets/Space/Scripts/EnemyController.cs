0b48a4a76b460550a906003f3184b35a1e00ebe4
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2023-03-08 15:19:13 -0500

    Refactor

--- Assets/Space/Scripts/EnemyController.cs
+++ Assets/Space/Scripts/EnemyController.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private GameObject player;
+    private const string PLAYER_TAG = "Player";
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        player = GameObject.FindWithTag(PLAYER_TAG);
         StartCoroutine(Fire());
     }
 
     void Update()
     {
-        if (player)
-        {
-            Vector2 direction = (gameObject.transform.position - player.transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+        if (!player) return;
 
-            transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
+        Vector2 direction = (gameObject.transform.position - player.transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
     }
 
     IEnumerator Fire()
@@ -28,8 +29,8 @@ public class EnemyController : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(1.3f);
-            if (!player)
-                break;
+            if (!player) break;
+
             GetComponent<LaserSystem>().Fire();
 
         }
